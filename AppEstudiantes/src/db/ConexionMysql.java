3ed84ca9b3bc7e01e3a8106c028b14ae777432e1
LuckScience <124910993+Gabote26@users.noreply.github.com>
2025-11-02 19:07:20 -0600

    Change Database

--- AppEstudiantes/src/db/ConexionMysql.java
+++ AppEstudiantes/src/db/ConexionMysql.java
@@ -5,27 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionMysql {
+    private static final String URL = 
+    		"jdbc:mysql://students-data-mysql-studentsdatamysql.c.aivencloud.com:11529/students_data_mysql"
+            + "?sslMode=REQUIRED"
+            + "&serverTimezone=UTC"
+            + "&connectTimeout=8000"
+            + "&socketTimeout=8000";
 
-	// Se reemplza con la url del tunnel
-	private static final String URL = "jdbc:mysql://skirts-joshua-epic-leather.trycloudflare.com:3306/students_data_mysql" + "?useSSL=false"
-			+ "&allowPublicKeyRetrieval=true" + "&serverTimezone=UTC";
-	private static final String USER = "root";
-	private static final String PASSWORD = "";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_X8ZLHz18HP2o0JB2eGv";
 
-	public Connection conectar() {
-		Connection cn = null;
-		try {
-			// Cargar driver de MySQL
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			// Se intenta establecer la conexión
-			cn = DriverManager.getConnection(URL, USER, PASSWORD);
-			System.out.println("✅ Conectado correctamente a la base de datos");
-		} catch (ClassNotFoundException e) {
-			System.err.println("❌ No se encontró el driver JDBC: " + e.getMessage());
-		} catch (SQLException e) {
-			System.err.println("❌ Error al conectar con la base de datos: " + e.getMessage());
-		}
-		return cn;
-	}
+    public Connection conectar() {
+        Connection cn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✅ Conectado correctamente a la base de datos Aiven MySQL");
+        } catch (ClassNotFoundException e) {
+            System.err.println("❌ No se encontró el driver JDBC: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("❌ Error al conectar con la base de datos: " + e.getMessage());
+        }
+        return cn;
+    }
 }
