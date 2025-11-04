4728cc696b259d17ac26d20070a9e01c5972c387
LuckScience <124910993+Gabote26@users.noreply.github.com>
2025-11-04 07:19:58 -0600

    Get

--- AppEstudiantes/src/guiProfesor/GestionarEstudiante.java
+++ AppEstudiantes/src/guiProfesor/GestionarEstudiante.java
@@ -10,27 +10,14 @@ public class GestionarEstudiante extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GestionarEstudiante frame = new GestionarEstudiante();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private MainForTeachers parentFrame;
 
 	/**
 	 * Create the frame.
 	 */
-	public GestionarEstudiante() {
+	public GestionarEstudiante(MainForTeachers parent) {
+		this.parentFrame = parent;
+		setTitle("Agregar Nuevo Estudiante");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
