3b3f4bd01d0a88d966ac9201a601fb21f8cba669
USUARIO <USUARIO@C2-003-EC>
2025-11-10 09:15:05 -0600

    Horarios Acabados

--- AppEstudiantes/src/guiEstudiante/HorariosEstudiantes.java
+++ AppEstudiantes/src/guiEstudiante/HorariosEstudiantes.java
@@ -2,6 +2,7 @@ package guiEstudiante;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.*;
@@ -14,6 +15,16 @@ public class HorariosEstudiantes extends JFrame {
     private JScrollPane scrol;
     private JLabel imagenLabel;
     private Map<String, String> mapaHorarios;
+
+    // Rutas CARPETAS IMAGENES
+    private final String[] rutasBase = {
+        "resources/1ER-SEMESTRE-MATU/",
+        "resources/1ER-SEMESTRE-VESP/",
+        "resources/3ER-SEMESTRE-MATU/",
+        "resources/3ER-SEMESTRE-VESP/",
+        "resources/5TO-SEMESTRE-MATU/",
+        "resources/5TO-SEMESTRE-VESP/"
+    };
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -49,7 +60,7 @@ public class HorariosEstudiantes extends JFrame {
         comboBox_grupo.setBounds(135, 50, 120, 30);
         contentPane.add(comboBox_grupo);
 
-        comboBox_turno = new JComboBox<>(new String[]{"AM", "VM"});
+        comboBox_turno = new JComboBox<>(new String[]{"AM", "AV", "VM"});
         comboBox_turno.setBounds(265, 50, 95, 30);
         contentPane.add(comboBox_turno);
 
@@ -57,7 +68,6 @@ public class HorariosEstudiantes extends JFrame {
         btnAceptar.setBounds(370, 50, 120, 30);
         contentPane.add(btnAceptar);
 
-        // Crear JLabel para imagen y colocarlo dentro del JScrollPane
         imagenLabel = new JLabel();
         imagenLabel.setHorizontalAlignment(JLabel.CENTER);
         imagenLabel.setVerticalAlignment(JLabel.TOP);
@@ -66,25 +76,82 @@ public class HorariosEstudiantes extends JFrame {
         scrol.setBounds(20, 100, 660, 450);
         contentPane.add(scrol);
 
+        comboBox_semestre.addActionListener(e -> actualizarHorario());
+        comboBox_grupo.addActionListener(e -> actualizarHorario());
+        comboBox_turno.addActionListener(e -> actualizarHorario());
         btnAceptar.addActionListener(e -> actualizarHorario());
 
         inicializarHorarios();
     }
-
+//RUTAS DE IMAGEN HORARIOS
     private void inicializarHorarios() {
         mapaHorarios = new HashMap<>();
+        //horarios 1 SEMESTRE MT
         mapaHorarios.put("1°-AMADMRH-AM", "1_AMADMRH_page-0001.jpg");
-        imagenLabel.setIcon(new ImageIcon("resources/1ER-SEMESTRE-MATU/1_AMADMRH_page-0001.jpg"));
         mapaHorarios.put("1°-BMADMRH-AM", "1_BMADMRH_page-0001.jpg");
-        imagenLabel.setIcon(new ImageIcon("resources/1ER-SEMESTRE-MATU/1_BMADMRH_page-0001.jpg"));
-        mapaHorarios.put("1°-CMADMRH-AM", "1_CMADMRH_page-0001.jpg");
-        imagenLabel.setIcon(new ImageIcon("resources/1ER-SEMESTRE-MATU/1-CMADMRH_page-0001.jpg"));
+        mapaHorarios.put("1°-CMADMRH-AM", "1-CMADMRH_page-0001.jpg");
         mapaHorarios.put("1°-PRO-AM", "1_AMPRO_page-0001.jpg");
         mapaHorarios.put("1°-ELE-AM", "1_AMELE_page-0001.jpg");
         mapaHorarios.put("1°-MEC-AM", "1_AMMEC_page-0001.jpg");
-        mapaHorarios.put("1°-SMEC-AM", "1_SMEC_page-0001.jpg");
+        mapaHorarios.put("1°-SMEC-AM", "1_AMSMEC_page-0001.jpg");
         mapaHorarios.put("1°-LOG-AM", "1_AMLOG_page-0001.jpg");
         mapaHorarios.put("1°-MAU-AM", "1_AMMAU_page-0001.jpg");
+        mapaHorarios.put("1°-LOG-AM", "1-AMLOG_page-0001.jpg");
+        //horarios 1 SEMESTRE VP
+        mapaHorarios.put("1°-AMADMRH-AV", "1_AVADMRH_page-0001.jpg");
+        mapaHorarios.put("1°-BMADMRH-AV", "1_BVADMRH_page-0001.jpg");
+        mapaHorarios.put("1°-PRO-AV", "1_AVPRO_page-0001.jpg");
+        mapaHorarios.put("1°-ELE-AV", "1-AVELE_page-0001.jpg");
+        mapaHorarios.put("1°-MEC-AV", "1_AVMEC_page-0001.jpg");
+        mapaHorarios.put("1°-MEC-AV", "1_BVMEC_page-0001.jpg");
+        mapaHorarios.put("1°-SMEC-AV", "1-AVSMEC_page-0001.jpg");
+        mapaHorarios.put("1°-LOG-AV", "1_AVLOG_page-0001.jpg");
+        mapaHorarios.put("1°-MAU-AV", "1_AVMAU_page-0001.jpg");
+        mapaHorarios.put("1°-LOG-AV", "1-AVLOG_page-0001.jpg");
+        //horarios 3 SEMESTRE MT
+        mapaHorarios.put("3°-AMADMRH-AM", "3_AMADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-BMADMRH-AM", "3_BMADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-CMADMRH-AM", "3_CMADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-PRO-AM", "3_AMPRO_page-0001.jpg");
+        mapaHorarios.put("3°-ELE-AM", "3-AMELE_page-0001.jpg");
+        mapaHorarios.put("3°-MEC-AM", "3_AMMEC_page-0001.jpg");
+        mapaHorarios.put("3°-SMEC-AM", "3-AMSMEC_page-0001.jpg");
+        mapaHorarios.put("3°-LOG-AM", "3_AMLOG_page-0001.jpg");
+        mapaHorarios.put("3°-MAU-AM", "3_AMMAU_page-0001.jpg");
+        mapaHorarios.put("3°-LOG-AM", "3-AMLOG_page-0001.jpg");
+        // HORARIOS 3 SEMESTRE VP
+        mapaHorarios.put("3°-AMADMRH-AV", "3_AVADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-BMADMRH-AV", "3_BVADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-CMADMRH-AV", "3-CVADMRH_page-0001.jpg");
+        mapaHorarios.put("3°-PRO-AV", "3_AVPRO_page-0001.jpg");
+        mapaHorarios.put("3°-ELE-AV", "3-AVELE_page-0001.jpg");
+        mapaHorarios.put("3°-MEC-AV", "3_AVMEC_page-0001.jpg");
+        mapaHorarios.put("3°-SMEC-AV", "3-AVSMEC_page-0001.jpg");
+        mapaHorarios.put("3°-LOG-AV", "3_AVLOG_page-0001.jpg");
+        mapaHorarios.put("3°-MAU-AV", "3_AVMAU_page-0001.jpg");
+        mapaHorarios.put("3°-LOG-AV", "3-AVLOG_page-0001.jpg");
+        // HORARIOS 5TO SEMESTRE MT
+        mapaHorarios.put("5°-AMADMRH-AM", "5_AMADMRH_page-0001.jpg");
+        mapaHorarios.put("5°-BMADMRH-AM", "5_BMADMRH_page-0001.jpg");
+        mapaHorarios.put("5°-CMADMRH-AM", "5-CMADMRH_page-0001.jpg");
+        mapaHorarios.put("5°-PRO-AM", "5_AMPRO_page-0001.jpg");
+        mapaHorarios.put("5°-ELE-AM", "5-AMELE_page-0001.jpg");
+        mapaHorarios.put("5°-MEC-AM", "5_AMMEC_page-0001.jpg");
+        mapaHorarios.put("5°-SMEC-AM", "5-AMSMEC_page-0001.jpg");
+        mapaHorarios.put("5°-LOG-AM", "5_AMLOG_page-0001.jpg");
+        mapaHorarios.put("5°-MAU-AM", "5_AMMAU_page-0001.jpg");
+        mapaHorarios.put("5°-LOG-AM", "5-AMLOG_page-0001.jpg");
+        // HORARIOS 5TO SEMESTRE VP
+        mapaHorarios.put("5°-AMADMRH-AV", "5_AVADMHR_page-0001.jpg");
+        mapaHorarios.put("5°-BMADMRH-AV", "5_BVADMRH_page-0001.jpg");
+        mapaHorarios.put("5°-PRO-AV", "5_AVPRO_page-0001.jpg");
+        mapaHorarios.put("5°-ELE-AV", "5_AVELE_page-0001.jpg");
+        mapaHorarios.put("5°-MEC-AV", "5_AVMEC_page-0001.jpg");
+        mapaHorarios.put("5°-SMEC-AV", "5_AVSMEC_page-0001.jpg");
+        mapaHorarios.put("5°-LOG-AV", "5-AVLOG_page-0001.jpg");
+        mapaHorarios.put("5°-MAU-AV", "5_AVMAU_page-0001.jpg");
+        mapaHorarios.put("5°-LOG-AV", "5-AVLOG_page-0001.jpg");
+
     }
 
     private void actualizarHorario() {
@@ -93,14 +160,42 @@ public class HorariosEstudiantes extends JFrame {
         String turno = (String) comboBox_turno.getSelectedItem();
 
         String clave = semestre + "-" + grupo + "-" + turno;
-        String rutaImagen = mapaHorarios.get(clave);
+        String nombreArchivo = mapaHorarios.get(clave);
 
-        if (rutaImagen != null) {
-            ImageIcon icon = new ImageIcon(rutaImagen);
-            imagenLabel.setIcon(icon);
+        if (nombreArchivo != null) {
+            boolean imagenCargada = false;
+
+            for (String ruta : rutasBase) {
+                String rutaCompleta = ruta + nombreArchivo;
+                File archivo = new File(rutaCompleta);
+
+                if (archivo.exists()) {
+                    ImageIcon originalIcon = new ImageIcon(rutaCompleta);
+                    Image originalImage = originalIcon.getImage();
+
+                    int ancho = imagenLabel.getWidth();
+                    int alto = imagenLabel.getHeight();
+
+                    if (ancho > 0 && alto > 0) {
+                        Image imagenEscalada = originalImage.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+                        imagenLabel.setIcon(new ImageIcon(imagenEscalada));
+                    } else {
+                        imagenLabel.setIcon(originalIcon);
+                    }
+
+                    imagenCargada = true;
+                    break;
+                }
+            }
+
+            if (!imagenCargada) {
+                imagenLabel.setIcon(null);
+                JOptionPane.showMessageDialog(this, "No se encontró el horario");
+            }
         } else {
             imagenLabel.setIcon(null);
-            JOptionPane.showMessageDialog(this, "No se encontró imagen para esa combinación.");
+            JOptionPane.showMessageDialog(this, "No se encontró el horario");
         }
     }
 }
+
